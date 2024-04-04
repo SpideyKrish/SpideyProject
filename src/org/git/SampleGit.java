937b899619232118daa6f1295613f57306f07580
SpideyKrish <krishkk91@gmail.com>
2024-04-04 13:10:52 +0530

    my work done

---
+++ src/org/git/SampleGit.java
@@ -0,0 +1,12 @@
+package org.git;
+
+public class SampleGit {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("working directory");
+		System.out.println("system directory");
+		System.out.println("local directory");
+		System.out.println("remote directory");
+	}
+}
